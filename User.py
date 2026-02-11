9727189edbb1d4b55c192a4979334e7a351eaee0
Kamivision <kamivision@gmail.com>
2026-02-11 17:51:40 -0600

    started UsersII but not understanding class methods and instance methods

--- User.py
+++ User.py
@@ -1,1 +1,56 @@
 # your improved User class goes here
+class User():
+    all_posts = {}
+    all_users = []
+    
+    
+    def __init__(self, name, email, drivers_license):
+        self.name = name
+        self.email = email
+        self.drivers_license = drivers_license
+        self.posts = []
+    
+    def __str__(self):
+        return f"{self.name}" 
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name_val:str):
+        if name_val.isalpha():
+            self._name = name_val.title()
+        else:
+            raise Exception("The name should be a `str` value")
+        
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, email_val):
+        if ('@' in email_val) and (".com" == email_val[-4:]):
+            self._email = email_val
+        else:
+            raise Exception("This isn't a valid email address") 
+    
+    @classmethod
+    def create_post(cls):
+        post = input("Please enter your post:\n")
+        new_post = cls
+        print(new_post)
+    
+     
+# calling on the class to create a user        
+def new_user():
+    user_info = {
+        "name": input("Please enter your name:\n"),
+        "email": input("Please enter your email:\n"),
+        "drivers_license": input("Please enter your license number:\n")
+    }   
+    new_user = User(**user_info)    
+    return f"{new_user} was created successfully"
+
+print(new_user())
+
